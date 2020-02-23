57cab60083a2d50a8fa02c2a50ba215b5dcc0568
aduarte95 <aduarte.roldan@gmail.com>
2020-02-23 12:36:52 -0600

    Password encryption

--- src/component/SignUpForm/SignUpForm.jsx
+++ src/component/SignUpForm/SignUpForm.jsx
@@ -2,22 +2,109 @@ import React, { useState } from 'react';
 import './SignUpForm.scss';
 import { Form, Button } from 'react-bootstrap';
 import FormHeader from '../shared/FormHeader/FormHeader';
+import { Redirect } from 'react-router-dom';
+import axios from 'axios';
+import qs from 'querystring';
+import crypto from 'crypto';
+
+const registerUrl = `http://localhost:8080/api/v1/user/register`;
+
+const requestBody = {
+  givenName: '',
+  lastName: '',
+  direction: '',
+  idNumber: '',
+  telephone: '',
+  cellphone: '',
+  canBeAddedByCellphone: false,
+  email: '',
+  username: '',
+  password: ''
+}
 
 function SignUpForm() {
 const [validated, setValidated] = useState(false);
+const [ isRegistered, setIsRegistered ] = useState(false);
 
-  const handleSubmit = event => {
+  function handleSubmit(event) {
     const form = event.currentTarget;
     if (form.checkValidity() === false) {
       event.preventDefault();
       event.stopPropagation();
+    } else {
+      event.preventDefault();
+      console.log(requestBody)
+     /* axios.post(registerUrl, qs.stringify(requestBody))
+          .then(  response => {
+            if(response.data === 100) {
+              setIsRegistered(true);
+            } else {
+              setIsRegistered(false);
+            }  
+          })
+          .catch(function (error) {
+            console.log(error);
+          });*/
     }
-
+    
     setValidated(true);
   };
 
+  function handleChange(event) {
+    switch(event.target.name) {
+      case 'firstName':
+        requestBody.givenName = event.target.value;
+      break;
+      case 'lastName':
+        requestBody.lastName = event.target.value;
+      break;
+      case 'idNumber':
+        requestBody.idNumber = event.target.value;
+      break;
+      case 'direction':
+        requestBody.direction = event.target.value;
+      break;
+      case 'telephone':
+        requestBody.telephone = event.target.value;
+      break;
+      case 'cellphone':
+        requestBody.cellphone = event.target.value;
+      break;
+      case 'canBeAddedByCellphone':
+        if(event.target.value !== "") {
+          requestBody.canBeAddedByCellphone = true;
+        } else {
+          requestBody.canBeAddedByCellphone = false;
+        }
+      break;
+      case 'email':
+        requestBody.email = event.target.value.toLowerCase();
+      break;
+      case 'username':
+        requestBody.username = event.target.value.toLowerCase();
+      break;
+      case 'password':
+        var hash = crypto.createHash("sha256")
+        .update(event.target.value)
+        .digest("hex");
+        requestBody.password = hash;
+      break;
+
+      default:
+        break;
+    }
+  }
+
+
   return (   
     <div className="sign-up-form-container d-flex justify-content-center">
+      {isRegistered && 
+        <Redirect 
+          to={{
+            pathname: '/login',
+            state: { username: requestBody.username }
+          }}>
+          </Redirect>}
       <div className="sign-up-form-container__wrapper">
         <FormHeader 
           title="Open up your Bankito account now"
@@ -27,7 +114,12 @@ const [validated, setValidated] = useState(false);
         <Form className="sign-up-form-container__form" noValidate validated={validated} onSubmit={handleSubmit}> 
             <Form.Group controlId="formFirstName">
                 <Form.Label>*First Name</Form.Label>
-                <Form.Control required type="text" placeholder="First Name" />
+                <Form.Control 
+                required 
+                name="firstName" 
+                type="text" 
+                placeholder="First Name"
+                onChange={handleChange} />
                 <Form.Control.Feedback type="invalid">
                   Please enter your first name.
                 </Form.Control.Feedback>
@@ -35,7 +127,12 @@ const [validated, setValidated] = useState(false);
 
             <Form.Group controlId="formLastName">
                 <Form.Label>*Last Name</Form.Label>
-                <Form.Control required type="text" placeholder="Last Name" />
+                <Form.Control 
+                required
+                name="lastName" 
+                type="text" 
+                placeholder="Last Name" 
+                onChange={handleChange}/>
                 <Form.Control.Feedback type="invalid">
                   Please enter your last name.
                 </Form.Control.Feedback>
@@ -43,7 +140,12 @@ const [validated, setValidated] = useState(false);
 
             <Form.Group controlId="formIdNumber">
                 <Form.Label>*Identity Number</Form.Label>
-                <Form.Control required type="text" placeholder="Identity Number" />
+                <Form.Control 
+                required 
+                name="idNumber"
+                type="number" 
+                placeholder="Identity Number" 
+                onChange={handleChange}/>
                 <Form.Control.Feedback type="invalid">
                   Please enter your identity number.
                 </Form.Control.Feedback>
@@ -51,7 +153,12 @@ const [validated, setValidated] = useState(false);
 
             <Form.Group controlId="formDirection">
                 <Form.Label>*Direction</Form.Label>
-                <Form.Control required type="text" placeholder="Direction" />
+                <Form.Control 
+                required
+                name="direction" 
+                type="text" 
+                placeholder="Direction" 
+                onChange={handleChange}/>
                 <Form.Control.Feedback type="invalid">
                   Please enter your direction.
                 </Form.Control.Feedback>
@@ -59,7 +166,11 @@ const [validated, setValidated] = useState(false);
 
             <Form.Group controlId="formTelephone">
                 <Form.Label>Telephone</Form.Label>
-                <Form.Control required type="number" placeholder="Telephone" />
+                <Form.Control
+                name="telephone"
+                 type="number" 
+                 placeholder="Telephone" 
+                 onChange={handleChange}/>
                 <Form.Control.Feedback type="invalid">
                   Please enter your telephone.
                 </Form.Control.Feedback>
@@ -67,19 +178,32 @@ const [validated, setValidated] = useState(false);
 
             <Form.Group controlId="formCellphone">
                 <Form.Label>Cellphone</Form.Label>
-                <Form.Control required type="number" placeholder="Cellphone" />
+                <Form.Control
+                name="cellphone"
+                 type="number" 
+                 placeholder="Cellphone" 
+                 onChange={handleChange}/>
                 <Form.Control.Feedback type="invalid">
                   Please enter your cellphone.
                 </Form.Control.Feedback>
             </Form.Group>
 
             <Form.Group controlId="formCanBeAddedByCellphone">
-                <Form.Check type="checkbox" label="Let me be added by cellphone number" />
+                <Form.Check
+                name="canBeAddedByCellphone"
+                 type="checkbox" 
+                 label="Let me be added by cellphone number" 
+                 onChange={handleChange}/>
             </Form.Group>    
 
             <Form.Group controlId="formEmail">
                 <Form.Label>*Email</Form.Label>
-                <Form.Control required type="email" placeholder="Email" />
+                <Form.Control 
+                required 
+                name="email"
+                type="email" 
+                placeholder="Email" 
+                onChange={handleChange}/>
                 <Form.Control.Feedback type="invalid">
                   Please enter your email.
                 </Form.Control.Feedback>
@@ -87,7 +211,12 @@ const [validated, setValidated] = useState(false);
 
             <Form.Group controlId="formUsername">
                 <Form.Label>*Username</Form.Label>
-                <Form.Control required type="text" placeholder="Username" />
+                <Form.Control 
+                required 
+                name="username"
+                type="text" 
+                placeholder="Username" 
+                onChange={handleChange}/>
                 <Form.Control.Feedback type="invalid">
                   Please enter your username.
                 </Form.Control.Feedback>
@@ -95,7 +224,12 @@ const [validated, setValidated] = useState(false);
 
             <Form.Group controlId="formPassword">
                 <Form.Label>*Password</Form.Label>
-                <Form.Control required type="password" placeholder="Password" />
+                <Form.Control 
+                required 
+                name="password"
+                type="password" 
+                placeholder="Password" 
+                onChange={handleChange}/>
                 <Form.Control.Feedback type="invalid">
                   Please enter your password.
                 </Form.Control.Feedback>
